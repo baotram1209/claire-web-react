a9566d36d657b95e473fbf20403dcd8b331c4bb2
baotram1209 <baotramtran.1209@gmail.com>
2022-06-22 11:35:53 +0200

    modified-contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,20 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 // import champ from '../assets/champ.JPG';
 const Contact = () => {
-  axios.defaults.headers.post['Content-Type'] = 'application/json';
-  axios
-    .post('https://formsubmit.co/ajax/your@email.com', {
-      name: 'FormSubmit',
-      message: "I'm from Devro LABS",
-    })
-    .then((response) => console.log(response))
-    .catch((error) => console.log(error));
   return (
     <>
-      {/* <div className="container container-contact">
+      <div className="container container-contact">
         <h1 className="page-header">Me contacter</h1>
         <div className="page-content page-content-basic"></div>
-      </div> */}
+      </div>
     </>
   );
 };
